af884540b7a73e1fafc640d6fc7f9b15b02c107f
heeyjoe <victorcosta.ifce@gmail.com>
2021-09-24 11:33:12 -0300

    Controllers e validações do cadastro de usuários

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:sucess] = 'Usuário cadastrado com sucesso'
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :name, :password, :password_confirmation)
   end
 
 end
